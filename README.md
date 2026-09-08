3bb4c66721c0889b161a63bfc55306b5716f4643
Daghan Dinc <ddaghan@gmail.com>
2026-09-09 00:33:31 +0200

    Convert to npm package

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-uuidv7 generator
+# A very small uuidv7 generator
+
+## Install
+
+```
+npm install uuidv7mini
+```
+
+## Use
 
 ```js
-import {uuidv7} from './uuidv7.js'
+import {uuidv7} from 'uuidv7'
 
 const u = uuidv7();
 console.log(u); // '01a082cb-168b-7150-bbae-60507e5033dc'
--- uuidv7.js
+++ uuidv7.js
@@ -1,1 +1,1 @@
-export function uuidv7(){return Date.now().toString(16).padStart(12,'0').replace(/(.{8})(.{4})/,'$1-$2')+'-7'+crypto.randomUUID().substring(15,36);}
+function e(){return Date.now().toString(16).padStart(12,`0`).replace(/(.{8})(.{4})/,`$1-$2`)+`-7`+crypto.randomUUID().substring(15,36)}export{e as uuidv7};
