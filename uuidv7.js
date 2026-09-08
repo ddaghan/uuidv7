4c9d6910f316946a99f3db941542923fff8c3746
Daghan <ddaghan@gmail.com>
2026-09-08 21:44:06 +0200

    shorten uuidv7 method

--- uuidv7.js
+++ uuidv7.js
@@ -1,3 +1,1 @@
-export function uuidv7() {
-    return Date.now().toString(16).padStart(12, '0').replace(/(.{8})(.{4})/,'$1-$2') + '-7' + crypto.randomUUID().substring(15,36);
-}
+export function uuidv7(){return Date.now().toString(16).padStart(12,'0').replace(/(.{8})(.{4})/,'$1-$2')+'-7'+crypto.randomUUID().substring(15,36);}
